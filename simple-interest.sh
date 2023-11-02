a5e6d99aea57673c2c9247a7c63ae55c8ca11964
azsnowman1313 <124106985+azsnowman1313@users.noreply.github.com>
2023-11-02 01:45:45 -0700

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
 # Do not use this in production. Sample purpose only.
 # Author: Upkar Lidder (IBM)
 # Additional Authors:
-# <your GitHub username>
+# azsnowman1313
 # Input:
 # p, principal amount
 # t, time period in years
